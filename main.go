aa366feb3bb9118cd7e84f6f128b1a6884ef6e78
charliemenke <charliemenke@me.com>
2021-12-02 14:49:04 -0700

    sorted lists

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
+	mergetwosortedlists "github.com/charliemenke/LeetCode/merge-two-sorted-lists"
 )
 
 func main() {
-	fmt.Println(strconv.FormatBool(validparentheses.IsValid(("(])"))))
-	//MergeTwoLists([]ListNode)
+	//fmt.Println(strconv.FormatBool(validparentheses.IsValid(("(])"))))
+
+	list1 := mergetwosortedlists.ListNode{
+		Val: 1,
+		Next: &mergetwosortedlists.ListNode{
+			Val: 2,
+			Next: &mergetwosortedlists.ListNode{
+				Val:  4,
+				Next: nil,
+			},
+		},
+	}
+	list2 := mergetwosortedlists.ListNode{
+		Val: 1,
+		Next: &mergetwosortedlists.ListNode{
+			Val: 3,
+			Next: &mergetwosortedlists.ListNode{
+				Val:  4,
+				Next: nil,
+			},
+		},
+	}
+
+	_ = mergetwosortedlists.MergeTwoLists(&list1, &list2)
 }
--- merge-two-sorted-lists/merge-two-sorted-lists.go
+++ merge-two-sorted-lists/merge-two-sorted-lists.go
@@ -6,6 +6,40 @@ type ListNode struct {
 }
 
 func MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil && list2 == nil {
+		return nil
+	}
+	toReturn := ListNode{Val: 0, Next: nil}
+	toReturnPointer := &toReturn
+	list1Pointer := list1
+	list2Pointer := list2
+	if list1 != nil {
+		for {
+			if (list1Pointer != nil && list2Pointer != nil) && list1Pointer.Val <= list2Pointer.Val {
+				toReturnPointer.Val = list1Pointer.Val
+				list1Pointer = list1Pointer.Next
+			} else if (list1Pointer != nil && list2Pointer != nil) && list1Pointer.Val > list2Pointer.Val {
+				toReturnPointer.Val = list2Pointer.Val
+				list2Pointer = list2Pointer.Next
+			} else {
+				if list1Pointer == nil {
+					toReturnPointer.Val = list2Pointer.Val
+					list2Pointer = list2Pointer.Next
+				} else {
+					toReturnPointer.Val = list1Pointer.Val
+					list1Pointer = list1Pointer.Next
+				}
+			}
+			if list1Pointer == nil && list2Pointer == nil {
+				break
+			} else {
+				toReturnPointer.Next = new(ListNode)
+				toReturnPointer = toReturnPointer.Next
+			}
 
-	return nil
+		}
+	} else {
+		return list2
+	}
+	return &toReturn
 }
